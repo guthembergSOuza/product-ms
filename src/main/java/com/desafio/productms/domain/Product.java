6e4f3d716115a0eeffc069640d93c86cfcf7bd65
guthemberg.souza <guthemberg.souza@orubetecnologia.com.br>
2021-04-17 22:23:06 -0300

    criacao de classe Product

---
+++ src/main/java/com/desafio/productms/domain/Product.java
@@ -0,0 +1,45 @@
+package com.desafio.productms.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "PRODUCT")
+@Builder
+@Setter
+@Getter
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+	@Column(name="NAME")
+	private String name;
+	@Column(name="DESCRIPTION")
+	private String description;
+	@Column(name="PRICE")
+	private Double price;
+
+	
+}
