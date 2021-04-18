e5ea3c8e66875728e0693bc2827bb3201a448de1
guthemberg.souza <guthemberg.souza@orubetecnologia.com.br>
2021-04-17 22:23:56 -0300

    criacao/ajuste de configuracoes da api

--- src/main/java/com/desafio/productms/ProductMsApplication.java
+++ src/main/java/com/desafio/productms/ProductMsApplication.java
@@ -2,8 +2,9 @@ package com.desafio.productms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ProductMsApplication {
 
 	public static void main(String[] args) {
